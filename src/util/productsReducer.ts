3f952394cf66c56fc3dd18d14b3e6cf732dcd744
tsanev96 <tsanev96@gmail.com>
2021-06-10 15:12:55 +0300

    productsReducer function moved to util, added type for product actions

---
+++ src/util/productsReducer.ts
@@ -0,0 +1,36 @@
+import { ProductActions } from "../actions/productActions";
+
+type Action = {
+  type: ProductActions;
+  payload: number;
+};
+
+type State = {
+  quantity: number;
+  price: number;
+};
+
+export const productsReducer = (state: State, action: Action): State => {
+  const { type, payload } = action;
+  const { quantity } = state;
+
+  switch (type) {
+    case ProductActions.Increment:
+      const incrementedQuantity = quantity + 1;
+      return {
+        ...state,
+        quantity: incrementedQuantity,
+        price: payload * incrementedQuantity,
+      };
+
+    case ProductActions.Decrement:
+      const decrementedQuantity = quantity === 1 ? quantity : quantity - 1;
+      return {
+        ...state,
+        quantity: decrementedQuantity,
+        price: payload * decrementedQuantity,
+      };
+    case ProductActions.AddToCart:
+      return state;
+  }
+};
